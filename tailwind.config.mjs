387ad226ec67cd444c8fdff4c91e8587e1a585c1
Fernando Castro <devfercastro@gmail.com>
2024-10-02 16:00:42 -0300

    feat(tailwind): add custom animations - fade-in and bounce-delayed

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  theme: {
+    extend: {
+      animation: {
+        'fade-in': 'fadeIn 2s forwards',
+        'bounce-delayed': 'fadeIn 2s forwards, bounce 2.5s infinite'
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: "0" },
+          '100%': { opacity: "1" },
+        },
+      },
+    },
+  },	plugins: [],
 };
