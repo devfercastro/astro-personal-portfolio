6a77d8aa2b7600f04348687d3cff6693cd36b18d
Fernando Castro <devfercastro@gmail.com>
2024-10-07 18:41:40 -0300

    feat(experience): refactor and style changes - add responsive desing for mobiles - extract card into reusable component - turn into island

---
+++ src/components/ui/ExperienceCard.tsx
@@ -0,0 +1,45 @@
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid";
+
+interface IExperienceCard {
+	experience: {
+		job: string;
+		company: string;
+		from: string;
+		to: string;
+		description: string;
+	};
+}
+
+export const ExperienceCard: React.FC<IExperienceCard> = ({ experience }) => {
+	const toggleActivate = (event: React.MouseEvent<HTMLDivElement>) => {
+		const element = event.currentTarget;
+		element.classList.add("bg-gray-800");
+		setTimeout(() => element.classList.remove("bg-gray-800"), 300);
+	};
+
+	return (
+		<div
+			className="cursor-pointer rounded-xl py-5 px-10 experience-card border border-gray-800 hover:border-white transition-all duration-100 lg:duration-300 group"
+			onClick={toggleActivate}
+		>
+			<header className="flex flex-row w-full justify-between mb-2">
+				<div className="flex flex-col">
+					<h2 className="text-lg font-semibold">
+						{experience.job} - {experience.company}
+					</h2>
+					<p className="text-base text-gray-500">
+						{experience.from} - {experience.to}
+					</p>
+				</div>
+				<ArrowTopRightOnSquareIcon
+					height={25}
+					width={25}
+					className="lg:opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+				/>
+			</header>
+			<p className="text-sm mb-2">{experience.description}</p>
+		</div>
+	);
+};
+
+export default ExperienceCard;
