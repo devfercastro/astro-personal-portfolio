88b65976b32e85e0c9ea4b3b4bef9a02ee0f8eda
Fernando Castro <devfercastro@gmail.com>
2024-10-10 21:12:02 -0300

    build(deps): remove react and react-dom - remove deps and types - remove react integration from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react()],
+	integrations: [tailwind()],
 	output: "server",
 	adapter: vercelServerless(),
 });
