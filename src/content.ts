424897967932cc3638f02e0d1ba1e0697000d744
Fernando Castro <devfercastro@gmail.com>
2024-10-16 20:40:02 -0300

    feat: centralize text content and ts types - add content.ts file to store all content text - add goblal.d.ts file to store all types

---
+++ src/content.ts
@@ -0,0 +1,29 @@
+export const welcomeContent = "Hello, I'm Fernando Castro.";
+
+export const descriptionContent = "I'm a sofware developer.";
+
+export const aboutContent: string[] = [
+	"My coding journey began in 2019, building small websites with HTML, CSS, and JavaScript for fun. Soon after, I explored backend development with Node.js, followed by frontend work with React. Things clicked when I dove into YouTube tutorials, showing me the complexity of real-world projects.",
+	"I later joined FreeCodeCamp, where I deepened my knowledge of algorithms, data structures, and programming while improving my English skills. In college, I landed my first developer role at Battlerol, working on a web RPG game called Meeple Combat, built with HTML, CSS, and JavaScript.",
+	"Challenging coding problems sparked my interest in algorithms, leading me to LeetCode and a deeper focus on data structures. Now, I'm building my own projects with Next.js and Astro, while actively contributing to the open-source community.",
+];
+
+export const experienceContent: ExperiencesItem[] = [
+	{
+		job: "Frontend Developer",
+		company: "Battlerol",
+		from: "2023",
+		to: "2024",
+		description: "Building role games with HTML, CSS, and JavaScript",
+		url: "https://www.matecito.co/battlerol.net",
+	},
+];
+
+export const projectsContent: ProjectsItem[] = [
+	{
+		title: "Portfolio",
+		description: "Personal portfolio built with Astro and Tailwind CSS",
+		image: "/projects/portfolio.png",
+		link: "https://github.com/devfercastro/personal-portfolio",
+	},
+];
---
+++ src/types/global.d.ts
@@ -0,0 +1,15 @@
+interface ExperiencesItem {
+	job: string;
+	company: string;
+	from: string;
+	to: string;
+	description: string;
+	url: string;
+}
+
+interface ProjectsItem {
+	title: string;
+	description: string;
+	image: string;
+	link: string;
+}
