05e8d6c5d60b3e32eb7c58d6ddb0f68c8c2a4ff2
Fernando Castro <devfercastro@gmail.com>
2024-10-20 13:29:57 -0300

    Update content.ts

--- src/content.ts
+++ src/content.ts
@@ -4,8 +4,8 @@ export const descriptionContent = "I'm a sofware developer.";
 
 export const aboutContent: string[] = [
 	"My coding journey began in 2019, building small websites with HTML, CSS, and JavaScript for fun. Soon after, I explored backend development with Node.js, followed by frontend work with React. Things clicked when I dove into YouTube tutorials, showing me the complexity of real-world projects.",
-	"I later joined FreeCodeCamp, where I deepened my knowledge of algorithms, data structures, and programming while improving my English skills. In college, I landed my first developer role at Battlerol, working on a web RPG game called Meeple Combat, built with HTML, CSS, and JavaScript.",
-	"Challenging coding problems sparked my interest in algorithms, leading me to LeetCode and a deeper focus on data structures. Now, I'm building my own projects with Next.js and Astro, while actively contributing to the open-source community.",
+	"I later joined FreeCodeCamp, where I deepened my knowledge of algorithms, data structures, and programming while improving my English skills. Also while in college, I landed my first developer role at Battlerol, working on a web RPG game called Meeple Combat, built with HTML, CSS, and JavaScript.",
+	"Challenging coding problems sparked my interest in algorithms, leading me to LeetCode and a deeper focus on data structures. Now, I'm building my own projects with Next.js and Astro, while doing some contributions to the open-source community.",
 ];
 
 export const experienceContent: ExperiencesItem[] = [
