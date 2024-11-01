78c9e9daa4dd3f4ea3b93e67137416abb122d56d
Fernando Castro <devfercastro@gmail.com>
2024-10-31 21:29:09 -0300

    chore(deps): Add preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercelServerless from "@astrojs/vercel/serverless";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     "/": "/es",
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), preact()],
   output: "server",
   adapter: vercelServerless(),
 });
