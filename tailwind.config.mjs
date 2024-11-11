1ac051b469619d04a4eaf58fdcfc4a462fcacdad
Fernando Castro <devfercastro@gmail.com>
2024-11-11 20:13:12 -0300

    feat(badge): add click animation
    - Add the --badge-bg-alt var for alternative color
    - Add a custom tailwindcss animation to tailwind config
    - Implement the animation in the component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,11 +8,17 @@ export default {
 			animation: {
 				"fade-in": "fadeIn 2s forwards",
 				"bounce-delayed": "fadeIn 2s forwards, bounce 2.5s infinite",
+				"badge-click": "badge-click 300ms forwards",
 			},
 			keyframes: {
 				fadeIn: {
 					"0%": { opacity: "0" },
 					"100%": { opacity: "1" },
+				},
+				"badge-click": {
+					"0%": { backgroundColor: "var(--badge-bg)" },
+					"50%": { backgroundColor: "var(--badge-bg-alt)" },
+					"100%": { backgroundColor: "var(--badge-bg)" },
 				},
 			},
 		},
